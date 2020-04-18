a76907628f7088934c2f521a4dcdeea34d88b7e7
Justin C. Hayes Douglas <58239589+justinHAYESdouglas@users.noreply.github.com>
2020-04-18 15:54:36 -0500

    Update contactform.php

--- scripts/contactform.php
+++ scripts/contactform.php
@@ -1,14 +1,10 @@
-<?php
-  if(isset($_POST['submit'])){
-    $name = $_POST['name'];
-    $subject = $_POST['subject'];
-    $mailFrom = $_POST['mail'];
-    $message = $_POST['message'];
-
-    $mailTo = "benlol14@yahoo.com";
-    $headers = "From: ".$mailFrom;
-    $txt = "You've got mail from ".$name.".\n\n".$message;
-
-    mail($mailTo, $subject, $txt, $headers);
-    header("Location: index.php?mailsend");
-  }
+<?php $name = $_POST['name'];
+$email = $_POST['email'];
+$message = $_POST['message'];
+$formcontent="From: $name \n Message: $message";
+$recipient = "justin@justinhayesdouglas.com";
+$subject = "Contact Form";
+$mailheader = "From: $email \r\n";
+mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
+echo "Thank You! The Email has been sent";
+?>
